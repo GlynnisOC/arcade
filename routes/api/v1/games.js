9cdc60aaea46bc6bdbfbe41eeedca174da49e003
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-28 20:56:54 -0600

    Add delete route but unable to see bug with exhausted eyes

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -51,13 +51,13 @@ router.post("/", function(req, res, next) {
 });
 
 
-router.put("/", function(req, res, next) {
+router.put("/:id", function(req, res, next) {
   Game.update({
           title: req.body.title,
           price: req.body.price,
           releaseYear: req.body.releaseYear,
           active: req.body.active
-    }
+    },
     {
       returning: true,
       where: {
@@ -74,7 +74,20 @@ router.put("/", function(req, res, next) {
     });
 });
 
-
+/* delete a game */
+router.delete("/:id", function(req, res, next) {
+  Game.destroy({
+      where: {id: req.params.id}
+    })
+    .then(game) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(204).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+})
 
 
 module.exports = router; //this should stay at the bottom of the file
